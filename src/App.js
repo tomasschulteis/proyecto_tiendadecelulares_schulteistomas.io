162127877aca7bfed35cc76e2453ae0503f9146c
tomasschulteis <tschulteis29@gmail.com>
2022-08-20 14:47:24 -0300

    version nueva

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
+
+
 function App() {
   return (
     <div className="App">
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect,useState } from 'react'
 import ItemCount from '../ItemCount';
 import ItemList from '../ItemList/ItemList';
+import ClipLoader from "react-spinners/ClipLoader"
 
 const productos = [
   {id: 1, imagen:"https://http2.mlstatic.com/D_NQ_NP_798686-MLA50207299147_062022-O.webp", marca:"Samsung" ,modelo: "Galaxy A23 128GB Negro", Precio: "$70000"},
@@ -8,7 +9,20 @@ const productos = [
   {id: 3,imagen:"https://http2.mlstatic.com/D_NQ_NP_2X_773268-MLA50421440914_062022-F.webp", marca:"Xiaomi" ,modelo: "REDMI NOTE 10", Precio: "$72900"}]
   
 
-export default function ItemListContainer({greeting}) {
+ export default function ItemListContainer({greeting}) {  
+    const [loading, setLoading] = useState(false)
+    useEffect(()=>{
+     setLoading(true)
+     setTimeout(() => {
+      setLoading (false)
+     
+    },2000)
+    
+  },[])
+  
+  
+  
+  
   const [data, setData] = useState ([]);
   
   useEffect(() => {
@@ -30,7 +44,8 @@ export default function ItemListContainer({greeting}) {
     <div>
      <span>{greeting}</span>
      <ItemCount initial={1} stock={10} onAdd={onAdd}/>
-     <ItemList data={data}/>
+     <ClipLoader color={'#e25a87'} loading={loading}  size={150} />
+     <ItemList  data={data}/>
     </div>
   );
 
