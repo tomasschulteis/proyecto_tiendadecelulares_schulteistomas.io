256a733742c32375a989cf8a156378a79aec2cee
tomasschulteis <tschulteis29@gmail.com>
2022-09-10 13:04:05 -0300

    version con cart view

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,16 +6,16 @@ import '../Cart/cart.css'
 
 export const Cart = () => {
    const {items,removeItem,clear} = useContext(Cartcontext);
-    
+ 
    return (<>
     <div className='titulo'>DETALLE DE MI COMPRA:</div>
      {!items.length ? <h2>sin articulos <Link to="/"><Button className='iniciobtn'>Volver</Button></Link></h2> :
      <>
          <ol>
-           {items.map(((item,indx)=><li key={indx}>{item.modelo} - {item.quantity}<Button className='botonnremover' onClick={()=>removeItem(item.id)}>Remover</Button></li>))}
+           {items.map(((item,indx)=><li key={indx}>{item.modelo}  ({item.quantity})  {item.Precio}<Button className='botonnremover' onClick={()=>removeItem(item.id)}>Remover</Button></li>))}
          </ol>
          <hr className='linea' ></hr>
-          <h3>Precio Final: = $ {items.reduce((pv,cv)=> pv + (cv.precio * cv.quantity),0)}</h3>
+          <h3 className='preciof'>Precio Final: = $ {parseFloat(items.reduce((pv,cv)=> pv + parseFloat(cv.Precio.replace("$","") * parseFloat(cv.quantity)),0))}</h3>
      </>
      }
      
